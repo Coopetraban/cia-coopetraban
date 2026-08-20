cebe63e366d51b75ecf84c584008c81d8c97b057
Coopetraban <coopetrabansedeprincipal@gmail.com>
2026-08-20 08:56:40 -0500

    supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 export const supabase = createClient(
-  'https://qljcyjfludkmfjzsvpzk.supabase.co',
-  'sb_publishable_coXAnuBJ4mi2xZ-kYm8q3A_7t1EC8le'
+  'https://lamfdtgvuybttjxzvnez.supabase.co',
+  'sb_publishable_B1YfVdLI7_RXaGM_sHMAkA_8zVCIqEn'
 );
